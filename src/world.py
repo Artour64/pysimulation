33075d49c5fec5bd983f67e2f338885d44de6a7a
Artour Konstantinov <artoukons@gmail.com>
2020-07-05 19:06:19 -0400

    added tile property classes

--- src/world.py
+++ src/world.py
@@ -3,10 +3,17 @@ worldY=24
 
 grid=list()
 
+class Entity:
+	pass
+
+class TileEnv:
+	pass
+
 class Tile:
 	x=0
 	y=0
-	pass
+	env=TileEnv()
+	entity=list()
 
 for x in range(worldX):
 	grid.append(list())
